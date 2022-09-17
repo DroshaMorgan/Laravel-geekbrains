c1da48bc37d38f70742184daf1b83a55023b6140
DroshaMorgan <daniil.dron2000@gmail.com>
2022-09-17 17:04:38 +0300

    Lesson 6

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,24 +11,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function getCategory(/*int $id = null*/): array
-    {
-        $category = [];
-        $faker = Factory::create();
-
-        /*if($id) {
-            return [
-                'categoryTitle'       => $faker->jobTitle(),
-            ];
-        }*/
-
-        for($i=1; $i<5; $i++) {
-            $category[$i] = [
-                'categoryTitle'       => $faker->jobTitle(),
-            ];
-        }
-
-        return $category;
-    }
 }
